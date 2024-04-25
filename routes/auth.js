0a2160c2532885c7f026651dde0d6c35f3db9254
Yash Arora <yash.1138@zenmonk.tech>
2024-04-25 12:37:45 +0530

    Implemented Login route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import { upload } from '../middlewares/upload.js';
-import { register } from '../controllers/auth.js'
-import { verifiedToken } from '../middlewares/auth.js';
+import { login, register } from '../controllers/auth.js'
 
 // Routes with files
 const router = express.Router();
-router.post('/auth/register', upload.single('picture'), register);
-
+router.post('/register', upload.single('picture'), register);
+router.post('/login', login)
 export default router;
